45c9d49d5d0c6a8565e061f9cf3b64250f13fd5b
zjhuang <846163332@qq.com>
2025-06-08 01:46:04 +0800

    添加资源地址

--- routes/system.ts
+++ routes/system.ts
@@ -13,6 +13,28 @@ router.post("/system/status", async (ctx) => {
   ctx.response.body = ResponseHandler.successRes(null, "更新成功");
 });
 
+router.post("/system/sourceApi", async (ctx) => {
+  const obj = await ctx.request.body.json();
+
+  await kv.set(["system", "sourceApi"], {
+    status: obj.sourceApi,
+  });
+
+  ctx.response.status = 201;
+  ctx.response.body = ResponseHandler.successRes(null, "资源地址更新成功");
+});
+
+router.post("/system/searchApi", async (ctx) => {
+  const obj = await ctx.request.body.json();
+
+  await kv.set(["system", "searchApi"], {
+    status: obj.searchApi,
+  });
+
+  ctx.response.status = 201;
+  ctx.response.body = ResponseHandler.successRes(null, "搜索地址更新成功");
+});
+
 router.get("/system/status", async (ctx) => {
   const data = await kv.get(["system", "status"]);
   ctx.response.status = 201;
